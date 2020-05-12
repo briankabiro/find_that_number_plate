a0aa53709a98025499ba82d2f45b3859bf82b496
briankabiro <briankigondu@gmail.com>
2020-05-12 07:07:55 +0300

    Add routes for counties controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
   post :search, to: "home#search"
+
+  get :index, to: "counties#index"
+  post :search, to: "counties#search"
 end
